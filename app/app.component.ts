9467e3bdb238139ed4c64556d84bafeae486c64e
Patrick Dawson <patrick.soboljew@gmail.com>
2016-08-11 20:50:34 +0200

    Initial project files import.

---
+++ app/app.component.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by patri_000 on 10.08.2016.
+ */
+
+import { Component } from '@angular/core';
+@Component({
+    selector: 'my-app',
+    template: '<h1>My First Angular 2 App!!!</h1>'
+})
+export class AppComponent { }
---
+++ app/main.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by patri_000 on 10.08.2016.
+ */
+
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app.module';
+platformBrowserDynamic().bootstrapModule(AppModule);
